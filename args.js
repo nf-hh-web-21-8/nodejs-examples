001050579b52e8011d0064cca45c12c0ae9a4f6b
Gregor Adams <greg@pixelass.com>
2022-01-25 09:23:04 +0100

    chore: adjust args example

--- args.js
+++ args.js
@@ -1,9 +1,20 @@
 import process from "node:process";
 
+// Version 2
 const getArgs = () => {
 	const [, , ...args] = process.argv;
 	return args;
 };
 
 const args = getArgs();
+
 console.log(args);
+
+// Version 2
+const getArgs2 = () => {
+	const args = process.argv.slice(2);
+};
+
+const args2 = getArgs2();
+
+console.log(args2);
